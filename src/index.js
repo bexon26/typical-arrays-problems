f7af425bb09334db4249a8ba22edf504dcd57e4c
bexon26 <47786103+bexon26@users.noreply.github.com>
2020-02-27 14:36:51 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ((array != 0) && (array !== undefined)){
+        var min = array[0];
+       for (var i=1; i<array.length; i++){
+            if (array[i]<min) {
+                min = array[i];
+            }
+        }
+       return min;  
+    } else {return 0}
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ((array != 0) && (array !== undefined)){
+        var max = array[0];
+        for (var i=1; i<array.length; i++){
+            if (array[i]>max) {
+                max = array[i];
+            }
+        }
+        return max;
+    } else {return 0}
 }
-
 exports.avg = function avg (array) {
-  return 0;
+    if ((array != 0) && (array !== undefined)){
+        var sum = 0;
+        for (var i=0; i<array.length; i++){
+            sum += array[i];
+        }
+        var average = sum/array.length
+    return average;
+    } else {return 0}
 }
